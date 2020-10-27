21ff87baa16e71d404ffd47f3be5d9f22ffc38ea
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 16:53:33 -0500

    small fix

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -1,4 +1,4 @@
-import React, { useContext, createContext, reducer, useReducer } from "react";
+import React, { useContext, createContext, useReducer } from "react";
 
 export const DataLayerContext = createContext();
 
--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,7 @@ function Login() {
     <div className="login">
       <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt="Spotify Login"
+        alt="Spotify Logo"
       />
       <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
