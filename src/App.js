1b1e3f1203387d520442a832e9c5144ea660d15e
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 15:05:04 -0500

    added spotify access token to spotify web api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Login from "./Login";
 import Player from "./Player";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -12,23 +15,21 @@ function App() {
     const hash = getTokenFromUrl();
 
     // removes visibility of token from the url
-    window.location.hash = ""; 
+    window.location.hash = "";
 
     const _token = hash.access_token;
 
-     if(_token)
-      setToken(_token);
+    if (_token) setToken(_token);
     // console.log("Token:", token);
-  }, []);     // using `[]` will run the code inside only once when App loads
 
-  return (
-    <div className="app">
-      {
-        token?(<Player />):(<Login />)
-      }
-      
-    </div>
-  );
+    spotify.setAccessToken(_token);
+
+    spotify.getMe().then((user) => {
+      console.log(user);
+    });
+  }, []); // using `[]` will run the code inside only once when App loads
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
