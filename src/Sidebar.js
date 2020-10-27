9e0adc91f44bfb31a1e69c1ea0c062212e3992aa
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 16:51:50 -0500

    added sidebar components

---
+++ src/Sidebar.js
@@ -0,0 +1,31 @@
+import { Home, LibraryMusic, Search } from "@material-ui/icons";
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify Logo"
+      />
+
+      <SidebarOption Icon={Home} title="Home" />
+      <SidebarOption Icon={Search} title="Search" />
+      <SidebarOption Icon={LibraryMusic} title="Your Library" />
+
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOption title="Hip Hop" />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="RnB" />
+      <SidebarOption title="Country" />
+    </div>
+  );
+}
+
+export default Sidebar;
