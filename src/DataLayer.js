6def19b1e9d353fc16db5ee183352db46a0445f1
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 16:06:38 -0500

    added a data layer to handle props

---
+++ src/DataLayer.js
@@ -0,0 +1,12 @@
+import React, { useContext, createContext, reducer, useReducer } from "react";
+
+export const DataLayerContext = createContext();
+
+// children = App (any sub-component inside DataLayer Component)
+export const DataLayer = ({ initialState, reducer, children }) => (
+  <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </DataLayerContext.Provider>
+);
+
+export const useDataLayerValue = () => useContext(DataLayerContext);
