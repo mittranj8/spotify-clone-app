45f49cb16ef8c7e108d55cf724fe0abe1927072a
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 19:07:05 -0500

    initial firebase api file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAOQ0htg7dLodrd1Wuw-7Sffcik_twTqCs",
+  authDomain: "spotify-clone-3b58f.firebaseapp.com",
+  databaseURL: "https://spotify-clone-3b58f.firebaseio.com",
+  projectId: "spotify-clone-3b58f",
+  storageBucket: "spotify-clone-3b58f.appspot.com",
+  messagingSenderId: "380411926394",
+  appId: "1:380411926394:web:d7a5e1b1fd5c958b2028c3",
+  measurementId: "G-QFXJV6T4DN",
+};
