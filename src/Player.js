78902a9e185fec12ff2e5976dc35f3890cb9f0c6
Mittranj <mpansuriya@unomaha.edu>
2020-10-27 19:06:16 -0500

    small fix

--- src/Player.js
+++ src/Player.js
@@ -11,7 +11,7 @@ function Player({ spotify }) {
     <div className="player">
       <div className="player_body">
         <Sidebar />
-        <Body />
+        <Body spotify={spotify} />
       </div>
       <Footer />
     </div>
